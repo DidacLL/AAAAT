7f266dd1359ffa5c6a95b22b35b7b0096f263c65
Didac Ll. <107514286+DidacLL@users.noreply.github.com>
2026-09-06 14:01:12 +0200

    Expose Focus preferences through preload

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -1,11 +1,16 @@
 import { contextBridge, ipcRenderer } from "electron";
 
 import { createDesktopApi } from "./api";
+import { createFocusDesktopApi } from "./focus-api";
 import { createTodoDesktopApi } from "./todo-api";
 
 const invoke = (channel: string, ...args: readonly unknown[]) => ipcRenderer.invoke(channel, ...args);
 
 contextBridge.exposeInMainWorld(
   "aaaat",
-  Object.freeze({ ...createDesktopApi(invoke), ...createTodoDesktopApi(invoke) }),
+  Object.freeze({
+    ...createDesktopApi(invoke),
+    ...createTodoDesktopApi(invoke),
+    ...createFocusDesktopApi(invoke),
+  }),
 );
